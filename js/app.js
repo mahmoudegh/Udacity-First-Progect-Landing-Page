ae27b41a3cf385422f1c0b99e173c85cab26a648
mahmoudegh <48510315+mahmoudegh@users.noreply.github.com>
2021-05-03 17:00:12 +0200

    Fix active section

--- js/app.js
+++ js/app.js
@@ -60,16 +60,16 @@ function handelActiveLinks() {
   }
 }
 
-// Checking if section is fully visible
+// Checking if section is partially visible
 function isInViewPort(element) {
   // Get the bounding client rectangle position in the viewport
   let bounding = element.getBoundingClientRect();
 
   if (
-    bounding.top >= 0 &&
+    bounding.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.bottom >= 0 &&
     bounding.left >= 0 &&
-    bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
-    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
   ) {
     return true;
   } else {
